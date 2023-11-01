4580e58a01bb19a46762a3ccdc2200ab5a8852ec
Marios <katzigkasm@gmail.com>
2023-11-01 21:58:09 +0200

    added sd logging support

--- rocket_test/rocket_test.ino
+++ rocket_test/rocket_test.ino
@@ -1,20 +1,46 @@
 #include <Arduino.h>
 #include "HX711.h"
+#include <SD.h>
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 const int IGNITER = 8;
-const int LED = 13;
+const int LED = 9;
+const int SD_ERROR = 7;
+const int SD_SPI_CS = 5;
 int i=0;
+float thrust = 0;
+int x = 0;
 
 HX711 scale;
+File myFile;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(SD_ERROR, OUTPUT);
   pinMode(IGNITER, OUTPUT);
   
   Serial.begin(57600);
+
+  if (!SD.begin(SD_SPI_CS)){
+    Serial.println("SD CARD FAILED OR NOT PRESENT");
+    digitalWrite(SD_ERROR, HIGH)
+    while (1);
+  }
+
+  if (!SD.exists("data.txt")){
+    Serial.println("data.txt does not exist, creating it now...");
+    myFile = SD.open("data.txt", FILE_WRITE);
+    myFile.close();
+  }
+
+  if (SD.exists("data.txt")){
+    Serial.println("data.txt found, continuing...");
+  }
+
+  myFile = SD.open("data.txt", FILE_WRITE);
+
  
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(-45364);
@@ -32,6 +58,13 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(scale.get_units());
+  thrust = scale.get_units();
+  Serial.println(thrust);
+  myFile.println(thrust);
+  x++;
+  if (x>49 && thrust<0.05){
+    myFile.close();
+    while (1);
+  }
   delay(50);
 }
